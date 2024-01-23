2fe2908b389b6bf10d66ee736b785b8ee3f81f4d
arthuravianna <arthur@DESKTOP-IKAO0FT>
2024-01-23 14:21:45 -0300

    feat: created CloseIcon.tsx for closeIcon svg

--- frontend/app/components/CartridgeInfo.tsx
+++ frontend/app/components/CartridgeInfo.tsx
@@ -30,6 +30,7 @@ import { fontPressStart2P } from '../utils/font';
 import { delay } from "../utils/util";
 import CheckIcon from "./svg/CheckIcon";
 import ErrorIcon from "./svg/ErrorIcon";
+import CloseIcon from "./svg/CloseIcon";
 
 
 enum STATUS {
@@ -66,9 +67,7 @@ function logFeedback(logStatus:LOG_STATUS, setLogStatus:Function) {
                     <div className="ms-3 text-sm font-normal">Invalid Log.</div>
                     <button onClick={click} type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
                         <span className="sr-only">Close</span>
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
+                        <CloseIcon/>
                     </button>
                 </div>
                 <div>
