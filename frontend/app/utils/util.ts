5b743f6738c1428f5a0a54ffccf397bf95193f54
arthuravianna <arthur@DESKTOP-C88PO09>
2024-03-27 10:10:41 -0300

    Revert "feat: privy setup"
    This reverts commit 93f8a1597fc3397eb210e0dbff9b89e1fcab2e8f.

--- frontend/app/utils/util.ts
+++ frontend/app/utils/util.ts
@@ -1,7 +1,4 @@
 import { useRef, useEffect } from 'react'
-import { base, mainnet, sepolia, polygon, polygonMumbai, Chain } from 'viem/chains';
-import { isHex, fromHex } from 'viem'
-
 
 export function delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
@@ -14,26 +11,4 @@ export function usePrevious(value: any) {
         ref.current = value;
     },[value]);
     return ref.current;
-}
-
-
-let chains:Record<number, Chain> = {};
-chains[base.id] = base;
-chains[mainnet.id] = mainnet;
-chains[sepolia.id] = sepolia;
-chains[polygon.id] = polygon;
-chains[polygonMumbai.id] = polygon;
-
-export function getChain(chainId:number):Chain;
-export function getChain(chainId:string):Chain;
-export function getChain(chainId:number|string) {
-    if (typeof chainId === "string") {
-        if (!isHex(chainId)) return null;
-        chainId = fromHex(chainId, "number");
-    }
-
-    const chain = chains[chainId];
-    if (!chain) return null;
-
-    return chain;
 }
