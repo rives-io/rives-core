8ad91dc0c3b951dbaee6566d38653bee1eee41f6
Arthur Vianna <arthurvianna@id.uff.br>
2024-04-22 12:32:26 -0300

    fix: responsiveness

--- frontend/app/tapes/page.tsx
+++ frontend/app/tapes/page.tsx
@@ -125,7 +125,7 @@ export default function Tapes() {
   return (
     <main>
       <section className="py-16 my-8 w-full flex justify-center">
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {
             verificationOutputs.map((verificationOutput, index) => {
               const cartridgeName = cartridgeInfoMap[verificationOutput.cartridge_id]?.name;
