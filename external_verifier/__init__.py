ffe77b6b155db2dc5124bacff0c00c3af5388829
Felipe Fink Grael <felipe@prototyp3.dev>
2024-04-29 16:32:42 -0300

    fix: Fix relative import in external_validator's init

--- external_verifier/__init__.py
+++ external_verifier/__init__.py
@@ -1,6 +1,6 @@
 from dagster import Definitions
 
-from services import verification_output_asset, \
+from .services import verification_output_asset, \
     verify_asset_job,initialize_storage_job,add_cartridge_job,add_rule_job,submit_verification_job, \
     inputs_sensor, initialization_sensor, submit_verification_sensor
 ###
