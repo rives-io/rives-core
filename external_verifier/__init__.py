8623445a9e1e40a9160bc3d76f27ceda3332b623
Felipe Fink Grael <felipe@prototyp3.dev>
2024-06-21 18:00:25 -0300

    fix: Add new assets to Definitions

--- external_verifier/__init__.py
+++ external_verifier/__init__.py
@@ -2,12 +2,12 @@ from dagster import Definitions
 
 from services import verification_output_asset, \
     verify_asset_job,initialize_storage_job,add_cartridge_job,add_rule_job,submit_verification_job, \
-    inputs_sensor, initialization_sensor, submit_verification_sensor
+    inputs_sensor, initialization_sensor, submit_verification_sensor,  add_cartridge_asset, add_rule_asset
 ###
 # Definitions
 
 defs = Definitions(
-    assets=[verification_output_asset],
+    assets=[verification_output_asset, add_cartridge_asset, add_rule_asset],
     jobs=[verify_asset_job,initialize_storage_job,add_cartridge_job,add_rule_job, submit_verification_job],
     sensors=[inputs_sensor,initialization_sensor,submit_verification_sensor],
 )
